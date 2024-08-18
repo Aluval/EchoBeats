809f1fb40a75122a6cab38c97fe758a5ebea0d92
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-08-18 19:49:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ import subprocess
 # Initialize the bot with your credentials
 api_id = '10811400'
 api_hash = '191bf5ae7a6c39771e7b13cf4ffd1279'
-bot_token = '7412278588:AAFKWhBga4p9sqXT9OcaYt41nQz14IVmQyA'
+bot_token = '7412278588:AAHmk19iP3uK79OglBISjicbl70TD6i9wEc'
 
 app = Client("slowreverb_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
