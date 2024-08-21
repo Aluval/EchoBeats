77456eac08d471ea5fb571c167b527aef8cac029
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-08-21 16:49:38 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

---
+++ config.py
@@ -0,0 +1,22 @@
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+import re
+from os import environ
+import os
+
+id_pattern = re.compile(r'^.\d+$')
+
+
+API_ID = os.environ.get("API_ID", "10811400")
+API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7412278588:AAHmk19iP3uK79OglBISjicbl70TD6i9wEc")
+ADMIN = int(os.environ.get("ADMIN", '6469754522'))
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://UPLOADXPRO24BOT:UPLOADXPRO24BOT@cluster0.hjfk60f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
+WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+PORT = int(os.environ.get("PORT", "8081"))
+SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID", "8a9eab1a1a2948fbaa582389e1ae565b")
+SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET", "e20f2cc4202146c3aa62ccf7ed83f80d")
