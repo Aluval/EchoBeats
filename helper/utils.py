88632856634c86695fc533bd2952df74303f2425
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-08-21 16:41:22 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- helper/utils.py
+++ helper/utils.py
@@ -1,7 +1,8 @@
-import os
+import os, re
 import ffmepg
 from pydub import AudioSegment
 from helper.ffmepg import run_ffmpeg_command
+from yt_dlp import YoutubeDL
 
 # Apply Slow Reverb effect function
 def apply_slowreverb(audio_path, output_path, room_size=0.75, damping=0.5, wet_level=0.08, dry_level=0.2, slowfactor=0.08):
@@ -59,3 +60,48 @@ def apply_8d_effect(audio_path, output_path):
     # Cleanup temporary file
     if audio_path == "tmp.wav":
         os.remove(audio_path)
+
+async def download_songs(music, download_directory="."):
+  query = f"{music}".replace("+", "")
+  ydl_opts = {
+      "format": "bestaudio/best",
+      "default_search": "ytsearch",
+      "noplaylist": True,
+      "nocheckcertificate": True,
+      "outtmpl": f"{music}.mp3",
+      "quiet": True,
+      "addmetadata": True,
+      "prefer_ffmpeg": True,
+      "geo_bypass": True,
+      "nocheckcertificate": True,
+  }
+
+  with YoutubeDL(ydl_opts) as ydl:
+      try:
+          video = ydl.extract_info(f"ytsearch:{music}", download=False)["entries"][0]["id"]
+          info = ydl.extract_info(video)
+          filename = ydl.prepare_filename(info)
+          if not filename:
+              print(f"Track Not Found⚠️")
+          else:
+              path_link = filename
+              return path_link, info 
+      except Exception as e:
+          raise Exception(f"Error downloading song: {e}") 
+          
+
+def get_access_token():
+    url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Authorization': f'Basic {credentials}',
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'client_credentials'
+    }
+    try:
+        response = requests.post(url, headers=headers, data=data)
+        response.raise_for_status()
+        return response.json().get('access_token')
+    except requests.RequestException as e:
+        raise Exception(f"Failed to get access token: {e}")
